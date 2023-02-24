5af9aeb573a6b50d80d5b1cc5bfc07ef9e306b3d
Piyush <piyushabhiranjan30@gmail.com>
2023-02-24 22:40:04 +0530

    Adding Cart Items

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 
 import Header from './components/Layout/Header';
-import { Fragment,useState } from 'react';
+import { useState } from 'react';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider'
 
 function App() {
 const [cartIsShown,setCartIsShown]=useState(false);
@@ -13,13 +14,13 @@ const [cartIsShown,setCartIsShown]=useState(false);
     setCartIsShown(false);
   }
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown&&<Cart onClose={hideCartHandler}/>}
       <Header onShowCart={showCartHandler}/>
       <main>
         <Meals/>
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
